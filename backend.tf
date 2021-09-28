256c1a1dff3a9175804bfad1a9d6bcfdaba4fcf5
akyol8957 <84547746+akyol8957@users.noreply.github.com>
2021-09-28 18:23:59 -0500

    adding comment for bucket name

--- backend.tf
+++ backend.tf
@@ -1,5 +1,6 @@
 terraform {
     backend "s3" {
+        ##update the bucket name which you have credentials to access
         bucket = "jenkins-terraform-evolvecybertraining"
         key = "terraform_vpc/us-east-1/class/dev/infrastructure.tfstate"
         region = "us-east-1"
